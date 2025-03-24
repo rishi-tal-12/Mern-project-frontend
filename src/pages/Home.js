8fc6fda1b2add09959064995fd336ef0ec18c61d
Rishi Talreja <rishi_t@me.iitr.ac.in>
2025-03-24 23:50:12 +0000

    This is frontend

---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import {useEffect,useState} from 'react'
+import {Link} from 'react-router-dom'
+const BACKEND_URL = "https://mern-project-2ougbeti7-rishi-tal-12s-projects.vercel.app"
+const Home =()=>{
+ const  [blogs,setblogs]=useState([])
+useEffect(()=>{
+    const fetchblogs = async()=>{ 
+   const response = await fetch(`${BACKEND_URL}/api/blogs`)
+   const json = await response.json()
+   if(response.ok){
+        setblogs(json)
+   }
+}
+   fetchblogs()
+},[])
+    return(
+        <div>
+            <div className="allblogs">
+            {blogs && blogs.map((blog)=>(
+                <div key={blog._id} >
+                   <Link to={`/blogss/${blog._id}`}>
+                    <h4>{blog.title}</h4>
+                </Link>
+                <p>{blog.content}</p>
+                <h4>{blog.author}</h4>
+           </div> ))}
+            </div>
+      </div>
+    )
+}
+
+export default Home
